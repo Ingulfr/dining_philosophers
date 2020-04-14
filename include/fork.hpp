4f6009a8397369a7d7ecffca8a802f0c0e8b0330
Ingulfr <konstantin.gevorkyan.personal@gmail.com>
2020-04-14 17:12:53 +0500

    Update to new project structure
    Fix namespace's name.
    Fix fork's class name.

--- include/fork.hpp
+++ include/fork.hpp
@@ -1,12 +1,11 @@
 #pragma once
 
 #include <mutex>
-#include <vector>
 
-namespace dining_philosophers::forks
+namespace entity
 {
 
-class fork_t
+class fork
 {
 public:
     bool take()
@@ -23,41 +22,4 @@ private:
     std::mutex locker;
 };
 
-
-
-class forks_distributor
-{
-public:        
-    forks_distributor( std::vector<fork_t> & forks )
-        : m_forks( forks ) , m_left_index( 0 )
-    {}        
-    
-    fork_t & left()
-    {
-        return m_forks[(m_left_index % 2) ? next_index_of( m_left_index ) : m_left_index];
-    }
-
-    fork_t & right()
-    {
-        return m_forks[(m_left_index % 2) ? m_left_index : next_index_of( m_left_index )];
-    }
-    
-    void next()
-    {
-        m_left_index = next_index_of( m_left_index );
-    }
-
-
- private:
-    size_t next_index_of( size_t i ) const
-    {
-        return (i + 1) % m_forks.size();
-    }
-        
-
-    std::vector<fork_t> & m_forks;
-
-    size_t m_left_index;
-};
-
-} // dining_philosolhers::forks
+} // namespace entity
