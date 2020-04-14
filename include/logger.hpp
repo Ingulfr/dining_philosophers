1e05389959bc2cea7e568d69bd22f96abc77a473
Ingulfr <konstantin.gevorkyan.personal@gmail.com>
2020-04-14 17:10:38 +0500

    Add wrapper into namespace logger

---
+++ include/logger.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+// we need to put 3rdpart/logger to namespace
+// so use this hack :
+
+namespace event_log
+{
+
+#include "include/3rdpart/Utils.hpp"
+
+using logger        = PhilosopherEventLog;
+using activity_type = ActivityType;
+
+} // namespace event_log
