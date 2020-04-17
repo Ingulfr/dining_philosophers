3a8a64dac3ce717dfc9ff47c7c8b9260ed3e1f50
Ingulfr <konstantin.gevorkyan.personal@gmail.com>
2020-04-17 21:24:23 +0500

    Fixed to use templates classes

--- src/dining_philosophers.cpp
+++ src/dining_philosophers.cpp
@@ -3,17 +3,20 @@
 
 #include <vector>
 
+#include "include/distributor.hpp"
 #include "include/philosopher.hpp"
 #include "include/table.hpp"
 
+using simple_dist = control::distributor<control::simple_logic>;
+using smart_dist = control::distributor<control::queue_hungers>;
 
-
-using philosophers_settings = entity::philosopher::settings;
+template<typename Distributor>
+using philosophers_settings = typename entity::philosopher<Distributor>::settings;
 
 using entity::make_philosophers_settings;
 
-
-std::vector<philosophers_settings> generate_philosophers_settings(size_t meals_remaining )
+template<typename Distributor>
+std::vector<philosophers_settings<Distributor>> generate_philosophers_settings(size_t meals_remaining )
 {
 
     static std::vector<std::string> names{ "Socrates" ,    "Plato",     "Aristotle",
@@ -25,25 +28,31 @@ std::vector<philosophers_settings> generate_philosophers_settings(size_t meals_r
     static std::vector< size_t> eating_minimum_times  { 30, 25, 10, 20,  20, 10 };
     static std::vector< size_t> eating_maximum_times  { 70, 80, 40, 90,  60, 90 };
 
-    std::vector<philosophers_settings> settings;
+    std::vector<philosophers_settings<Distributor>> settings;
 
     settings.reserve( names.size() );
 
     for ( auto i = 0u; i < names.size(); ++i )
     {
-        settings.emplace_back( make_philosophers_settings( names[i], std::uniform_int_distribution<>( thinking_minimum_times[i], thinking_maximum_times[i]),
-                               std::uniform_int_distribution<>( eating_minimum_times[i], eating_maximum_times[i]), i, meals_remaining ) );
+        settings.emplace_back( make_philosophers_settings<Distributor>( names[i], 
+                                                                        std::uniform_int_distribution<>( thinking_minimum_times[i],
+                                                                                                         thinking_maximum_times[i] ),
+                                                                        std::uniform_int_distribution<>( eating_minimum_times[i], 
+                                                                                                         eating_maximum_times[i] ), 
+                                                                        i, 
+                                                                        meals_remaining ) );
     }
 
     return settings;
 }
 
+template<typename DistributingStrategy>
 void organize_dinner(const int count_philosophers = 3, const size_t meals_remaining = 5)
 {
 
-    std::vector<philosophers_settings> phil_settings( generate_philosophers_settings( count_philosophers ) );
+    std::vector<philosophers_settings<control::distributor<DistributingStrategy>>> phil_settings( generate_philosophers_settings<control::distributor<DistributingStrategy>>( meals_remaining ) );
 
-    control::table table( phil_settings.begin(), phil_settings.begin() + count_philosophers);
+    control::table<DistributingStrategy> table( phil_settings.begin(), phil_settings.begin() + count_philosophers);
 
     table.start_dinner();
 
@@ -52,5 +61,8 @@ void organize_dinner(const int count_philosophers = 3, const size_t meals_remain
 
 int main()
 {
-    organize_dinner(5);
+    std::cout << "queue of hungers logic:\n";
+    organize_dinner<control::queue_hungers>(5);
+    std::cout << "\n\nsimple logic:\n";
+    organize_dinner<control::simple_logic>( 5 );
 }
