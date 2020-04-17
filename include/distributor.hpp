50b5cf5beaaa8fe2b5f9efd1c0e9dab2d3a77051
Ingulfr <konstantin.gevorkyan.personal@gmail.com>
2020-04-17 21:12:07 +0500

    Make distributor templates
    Add strategy for template distributor

--- include/distributor.hpp
+++ include/distributor.hpp
@@ -2,28 +2,63 @@
 
 #include <atomic>
 #include <functional>
+#include <type_traits>
 #include <vector>
 
 #include "include/details/unique_take.hpp"
 #include "include/fork.hpp"
 
+namespace control::details
+{
+
+template<bool Condition, typename Type>
+struct conditional_member
+{
+    conditional_member( ) = default;
+
+    conditional_member(Type && other ) 
+        : value(std::move(other ) )
+    { }
+
+    Type value;
+};
+
+} // namespace control::details
+
 namespace control
 {
 
+struct simple_logic { };
+
+struct queue_hungers { };
+
+//, typename = std::enable_if_t<std::is_same_v<DistributingStrategy, queue_hungers>>
+template<typename DistributingStrategy>
 class distributor
 {
+private:
+    using is_queue_hungers = std::is_same<DistributingStrategy, queue_hungers>;
+
+    using queue = std::vector<std::atomic_size_t>;
+
 public:
     class forks
     {
     public:
-        forks( ) = default;
+        template<typename = std::enable_if_t<is_queue_hungers::value>>
+        forks( )
+            : m_left( ),
+              m_right( ),
+              m_is_taken( false ),
+              m_on_destruct()
+        { }
 
-
+        template<typename = std::enable_if_t<is_queue_hungers::value>>
         forks( entity::fork & left, entity::fork & right, std::function<void( void )> callback )
             : m_left( ),
               m_right( ),
               m_is_taken( false ),
-              m_on_destruct( callback )
+              m_on_destruct( std::move(callback) )
         {
             details::unique_take take_left( left );
             details::unique_take take_right( right );
@@ -37,11 +72,32 @@ public:
             }
         }
 
+        template<typename = std::enable_if_t<!is_queue_hungers::value>>
+        forks( entity::fork & left, entity::fork & right)
+            : m_left( ),
+            m_right( ),
+            m_is_taken( false )
+        {
+            details::unique_take take_left( left );
+            details::unique_take take_right( right );
+
+            if ( take_left && take_right )
+            {
+                m_left = std::move( take_left );
+                m_right = std::move( take_right );
+                m_is_taken = true;
+
+            }
+        }
+
         ~forks( )
         {
-            if( m_on_destruct && m_is_taken )
+            if constexpr( is_queue_hungers::value )
             {
-                m_on_destruct( );
+                if ( m_on_destruct.value && m_is_taken )
+                {
+                    m_on_destruct.value( );
+                }
             }
         }
 
@@ -54,27 +110,43 @@ public:
         details::unique_take m_left;
         details::unique_take m_right;
 
-        bool m_is_taken = false;;
+        bool m_is_taken = false;
 
-        std::function<void( void )> m_on_destruct;
+        control::details::conditional_member<is_queue_hungers::value, std::function<void( void )>> m_on_destruct;
     };
 
 public:
     distributor( std::vector<entity::fork> & forks )
         : m_forks( forks ),
-          m_eat_queue( m_forks.size( ) )
+          m_eat_queue( queue(m_forks.size( ) ) )
     { }
 
+
+    //std::enable_if<is_queue_hungers::value, forks> take_forks( size_t phil_index)
     forks take_forks( size_t phil_index )
     {
-        bool can_take = check_left_phil( phil_index );
-        if ( can_take )
+        if constexpr ( is_queue_hungers::value )
         {
-            return { left( phil_index ), right( phil_index ), [this, phil_index]( ) { ++m_eat_queue[phil_index]; } };
+            bool can_take = check_left_phil( phil_index );
+
+            if ( can_take )
+            {
+                return { left( phil_index ), right( phil_index ), [this, phil_index]( ) { ++m_eat_queue.value[phil_index]; } };
+            }
+
+            return { };
         }
-        
-        return { };
+        else
+        {
+            return { left( phil_index ), right( phil_index ) };
+        }
     }
+
+    /*std::enable_if<!is_queue_hungers::value, forks> take_forks( size_t phil_index)
+    {
+        return { left( phil_index ), right( phil_index ) };
+    }*/
+
 
 private:
     size_t next_index_of( size_t i ) const
@@ -82,6 +154,7 @@ private:
         return (i + 1) % m_forks.size( );
     }
 
+    template<typename = std::enable_if_t<is_queue_hungers::value>>
     size_t prev_index_of( size_t i ) const
     {
         return (i - 1 + m_forks.size( )) % m_forks.size( );
@@ -97,15 +170,16 @@ private:
         return m_forks[(phil_index % 2) ? phil_index : next_index_of( phil_index )];
     }
 
+    template<typename = std::enable_if_t<is_queue_hungers::value>>
     bool check_left_phil( size_t phil_ind )
     {
-        return (m_eat_queue[prev_index_of( phil_ind )].load( std::memory_order_acquire ) >=
-                m_eat_queue[phil_ind].load( std::memory_order_acquire ));
+        return (m_eat_queue.value[prev_index_of( phil_ind )].load( std::memory_order_acquire ) >=
+                m_eat_queue.value[phil_ind].load( std::memory_order_acquire ));
     }
 
     std::vector<entity::fork> & m_forks;
 
-    std::vector<std::atomic_size_t> m_eat_queue;
+    control::details::conditional_member<is_queue_hungers::value, queue>  m_eat_queue;
 };
 
 } // namespace control
