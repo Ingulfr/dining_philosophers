623b3df05b6bc62354cccb1202ff0a0f621bcc5c
Ingulfr <konstantin.gevorkyan.personal@gmail.com>
2020-04-18 13:15:18 +0500

    Rename old distributor file
    Add using for more easy using distributors

---
+++ include/distributors.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "include/distributors/base_distributor.hpp"
+#include "include/distributors/queue_distributor.hpp"
+
+namespace control
+{
+
+using distributor  = base_distributor;
+
+using qdistributor = queue_distributor;
+
+} // namespace control
